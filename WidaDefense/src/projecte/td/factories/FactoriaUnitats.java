be7e9197ac817aa43217fe3da122b5e84c137538
lordzuzen <lordzuzen@41fc50e0-9c97-0efb-add9-b61474c962b3>
2010-05-19 16:39:13 +0000

    S'inclou una nova unitat amiga Bomba Aerea

--- WidaDefense/src/projecte/td/factories/FactoriaUnitats.java
+++ WidaDefense/src/projecte/td/factories/FactoriaUnitats.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import projecte.td.managers.ManagerRecursos;
 import projecte.td.domini.*;
 import projecte.td.utilitats.*;
- 
+
 /**
  *
  * @author wida47645633
@@ -12,16 +12,16 @@ import projecte.td.utilitats.*;
 public class FactoriaUnitats {
 
     private static ArxiuConfiguracio unitats;
-    private static HashMap<String,UnitatAbstract> llistaUnitats;
+    private static HashMap<String, UnitatAbstract> llistaUnitats;
 
     public static void creaHashMap() {
         if (unitats == null) {
             unitats = Configuracio.getUnitats();
         }
-        llistaUnitats = new HashMap<String,UnitatAbstract>();
+        llistaUnitats = new HashMap<String, UnitatAbstract>();
         UnitatAbstract ua = new Miner(unitats.getPropietatInt("vidaMiner"), unitats.getPropietatInt("cadenciaMiner"),
-                    unitats.getPropietatInt("capacitatMiner"), ManagerRecursos.getImage("minerImage"),
-                    ManagerRecursos.getImageArray("minerAnimation"));
+                unitats.getPropietatInt("capacitatMiner"), ManagerRecursos.getImage("minerImage"),
+                ManagerRecursos.getImageArray("minerAnimation"));
         llistaUnitats.put("Miner", ua);
 
     }
@@ -48,11 +48,11 @@ public class FactoriaUnitats {
             bo = new Miner(unitats.getPropietatInt("vidaMiner"), unitats.getPropietatInt("cadenciaMiner"),
                     unitats.getPropietatInt("capacitatMiner"), ManagerRecursos.getImage("minerImage"),
                     ManagerRecursos.getImageArray("minerAnimation"));
-        } else if (tipus.equals("Escut")) { 
+        } else if (tipus.equals("Escut")) {
             bo = new UnitatAbstract(100, ManagerRecursos.getImage("escutImage"),
                     ManagerRecursos.getImageArray("escutAnimation"));
         } else if (tipus.equals("Bomba")) {
-            bo = new Bomba(10000,ManagerRecursos.getImage("bombaImage"),
+            bo = new Bomba(10000, ManagerRecursos.getImage("bombaImage"),
                     ManagerRecursos.getImageArray("bombaAnimation"),
                     new ProjectilEstatic(20, ManagerRecursos.getImageArray("bombaProjectilAnimation")));
         } else if (tipus.equals("Caixa")) {
@@ -64,14 +64,18 @@ public class FactoriaUnitats {
                     ManagerRecursos.getImageArray("focAnimation"),
                     new ProjectilEstatic(0.25, ManagerRecursos.getImageArray("focProjectilAnimation")), -2, -5);
         } else if (tipus.equals("Mina")) {
-            bo = new Mina(200,ManagerRecursos.getImage("minaImage"),
+            bo = new Mina(200, ManagerRecursos.getImage("minaImage"),
                     ManagerRecursos.getImageArray("minaAnimation"),
                     new ProjectilEstatic(20, ManagerRecursos.getImageArray("minaProjectilAnimation")));
         } else if (tipus.equals("Motorista")) {
-            bo = new Motorista(100,ManagerRecursos.getImage("motoristaImage"),
+            bo = new Motorista(100, ManagerRecursos.getImage("motoristaImage"),
                     ManagerRecursos.getImageArray("motoristaAnimation"),
-                    new ProjectilAnimat(200,ManagerRecursos.getImage("motoristaImage"),
+                    new ProjectilAnimat(200, ManagerRecursos.getImage("motoristaImage"),
                     ManagerRecursos.getImageArray("motoristaAnimation")));
+        } else if (tipus.equals("BombaAerea")) {
+            bo = new BombaAerea(100, ManagerRecursos.getImage("bombaAereaImage"),
+                    ManagerRecursos.getImageArray("bombaAereaAnimation"),
+                    new ProjectilEstatic(20, ManagerRecursos.getImageArray("minaProjectilAnimation")));
         }
         return bo;
     }
@@ -81,32 +85,27 @@ public class FactoriaUnitats {
         if (tipus.equals("Momia")) {
             dolent = new UnitatEnemigaAtkDistancia(100, 2000, ManagerRecursos.getImage("pistolerImage"),
                     ManagerRecursos.getImageArray("momiaCaminaAnimation"),
-                    new ProjectilEstatic(0.10, ManagerRecursos.getImageArray("momiaProjectilAnimation")), ManagerRecursos.getImageArray("momiaAtacaAnimation"),0.030, -2, 4);
+                    new ProjectilEstatic(0.10, ManagerRecursos.getImageArray("momiaProjectilAnimation")), ManagerRecursos.getImageArray("momiaAtacaAnimation"), 0.030, -2, 4);
         } else if (tipus.equals("Natiu")) {
             dolent = new UnitatEnemigaAtkNormal(100, ManagerRecursos.getImage("pistolerImage"),
                     ManagerRecursos.getImageArray("natiuCaminaAnimation"),
-                    ManagerRecursos.getImageArray("natiuAtacaAnimation"),0.050, 0.25);
-        }
-        else if (tipus.equals("Espasa")) {
+                    ManagerRecursos.getImageArray("natiuAtacaAnimation"), 0.050, 0.25);
+        } else if (tipus.equals("Espasa")) {
             dolent = new UnitatEnemigaAtkNormal(100, ManagerRecursos.getImage("pistolerImage"),
                     ManagerRecursos.getImageArray("espasaCaminaAnimation"),
-                    ManagerRecursos.getImageArray("espasaAtacaAnimation"),0.060,0.25);
-        }
-        else if (tipus.equals("Ufo")) {
+                    ManagerRecursos.getImageArray("espasaAtacaAnimation"), 0.060, 0.25);
+        } else if (tipus.equals("Ufo")) {
             dolent = new UnitatEnemigaAtkDistancia(100, 2000, ManagerRecursos.getImage("pistolerImage"),
                     ManagerRecursos.getImageArray("ufoAnimation"),
-                    new ProjectilEstatic(0.50, ManagerRecursos.getImageArray("ufoProjectilAnimation")),ManagerRecursos.getImageArray("ufoAnimation"),0.08,-2,20);
-        }
-
-        else if (tipus.equals("Cranc")) {
+                    new ProjectilEstatic(0.50, ManagerRecursos.getImageArray("ufoProjectilAnimation")), ManagerRecursos.getImageArray("ufoAnimation"), 0.08, -2, 20);
+        } else if (tipus.equals("Cranc")) {
             dolent = new UnitatEnemigaAtkNormal(100, ManagerRecursos.getImage("pistolerImage"),
                     ManagerRecursos.getImageArray("crancCaminaAnimation"),
-                    ManagerRecursos.getImageArray("crancAtacaAnimation"),0.020,1);
-        }
-        else if (tipus.equals("Robot")) {
+                    ManagerRecursos.getImageArray("crancAtacaAnimation"), 0.020, 1);
+        } else if (tipus.equals("Robot")) {
             dolent = new UnitatEnemigaAtkDistanciaSalta(100, 2000, ManagerRecursos.getImage("pistolerImage"),
                     ManagerRecursos.getImageArray("robotCaminaAnimation"),
-                    new ProjectilEstatic(0.50, ManagerRecursos.getImageArray("robotProjectilAnimation")),ManagerRecursos.getImageArray("robotAtacaAnimation"),0.030,-2,20,2000);
+                    new ProjectilEstatic(0.50, ManagerRecursos.getImageArray("robotProjectilAnimation")), ManagerRecursos.getImageArray("robotAtacaAnimation"), 0.030, -2, 20, 2000);
         }
         return dolent;
     }
