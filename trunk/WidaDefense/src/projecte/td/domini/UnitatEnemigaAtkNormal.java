99692ccc95ed451d2ad684ccc4d147210ece85a0
ernest89 <ernest89@41fc50e0-9c97-0efb-add9-b61474c962b3>
2010-06-09 17:38:00 +0000

    invisible+comentaris

--- trunk/WidaDefense/src/projecte/td/domini/UnitatEnemigaAtkNormal.java
+++ trunk/WidaDefense/src/projecte/td/domini/UnitatEnemigaAtkNormal.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package projecte.td.domini;
 
 import org.newdawn.slick.GameContainer;
@@ -9,31 +5,63 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 
 /**
- *
- * @author wida47909974
+ * Classe UnitatEnemigaAtkNormal: Unitat enemiga que ataca cos a cos.
+ * @author Ernest Daban i David Alvarez
  */
 public class UnitatEnemigaAtkNormal extends UnitatEnemiga {
 
-    double dany;
+    double dany;//Dany de l'atac
 
+    /**
+     * Constructor UnitatEnemigaAtkNormal
+     * @param vida
+     * @param frames
+     * @param framesMort
+     * @param milisegons
+     * @param frames2
+     * @param velocitat
+     * @param milisegonsAtck
+     * @param dany
+     */
     public UnitatEnemigaAtkNormal(int vida, Image[] frames, Image[] framesMort, int milisegons, Image[] frames2, double velocitat, int milisegonsAtck, double dany) {
         super(vida, frames, framesMort, milisegons, frames2, velocitat, milisegonsAtck);
         this.dany = dany;
     }
 
+    /**
+     * Constructor UnitatEnemigaAtkNormal
+     * @param vida
+     * @param frames
+     * @param framesMort
+     * @param milisegons
+     * @param velocitat
+     * @param dany
+     */
     public UnitatEnemigaAtkNormal(int vida, Image[] frames, Image[] framesMort, int milisegons, double velocitat, double dany) {
         super(vida, frames, framesMort, milisegons, velocitat);
         this.dany = dany;
     }
 
+    /**
+     * Getter activat
+     * @return true si està activat
+     */
     public boolean isActivat() {
         return activat;
     }
 
+    /**
+     * Setter activat
+     * @param activat
+     */
     public void setActivat(boolean activat) {
         this.activat = activat;
     }
 
+    /**
+     * Getter dany
+     * @return dany
+     */
     public double getDany() {
         if (!mort) {
             return dany;
@@ -42,6 +70,11 @@ public class UnitatEnemigaAtkNormal extends UnitatEnemiga {
         }
     }
 
+    /**
+     * Dibuixa l'unitat
+     * @param gc GameContainer
+     * @param g Graphics
+     */
     @Override
     public void render(GameContainer gc, Graphics g) {
         renderVida(gc, g);
--- trunk/WidaDefense/src/projecte/td/domini/UnitatEnemigaExplosio.java
+++ trunk/WidaDefense/src/projecte/td/domini/UnitatEnemigaExplosio.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package projecte.td.domini;
 
 import java.awt.event.ActionEvent;
@@ -12,43 +8,64 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 
 /**
- *
- * @author wida47909974
+ * Classe UnitatEnemigaExplosio: Unitat enemiga que explosiona quan té contacte amb una unitat Amiga.
+ * @author Ernest Daban i David Alvarez
  */
 public class UnitatEnemigaExplosio extends UnitatEnemigaAtkNormal {
 
     boolean cremant;
     Timer timer;
 
+    /**
+     * Constructor UnitatEnemigaExplosio
+     * @param vida
+     * @param frames
+     * @param framesMort
+     * @param milisegons
+     * @param frames2
+     * @param velocitat
+     * @param milisegonsAtck
+     * @param dany
+     */
     public UnitatEnemigaExplosio(int vida, Image[] frames, Image[] framesMort, int milisegons, Image[] frames2, double velocitat, int milisegonsAtck, double dany) {
         super(vida, frames, framesMort, milisegons, frames2, velocitat, milisegonsAtck, dany);
         timer = new Timer(6000, new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
-                mort=true;
+                mort = true;
                 timer.stop();
             }
         });
     }
 
+    /**
+     * Actualitza la unitat
+     * @param delta
+     */
     @Override
     public void update(int delta) {
-        if(posX<=0-getWidth()/2){
-            haArribat=true;
+        if (posX <= 0 - getWidth() / 2) {
+            haArribat = true;
         }
         posX -= velocitat * delta;
         setLocation(posX, posY);
-        if(cremant && !timer.isRunning() && !mort){
+        //Si esta cremant, no està mort i el timer no s'ha iniciat; inicia el timer.
+        if (cremant && !timer.isRunning() && !mort) {
             timer.start();
         }
     }
 
+    /**
+     * Dibuixa la unitat
+     * @param gc GameContainer
+     * @param g Graphics
+     */
     @Override
     public void render(GameContainer gc, Graphics g) {
         renderVida(gc, g);
         if (activat || cremant) {
-            cremant=true;
-            g.drawAnimation(atck, posX, posY+animation.getHeight()-atck.getHeight());
+            cremant = true;
+            g.drawAnimation(atck, posX, posY + animation.getHeight() - atck.getHeight());
 
         } else {
             g.drawAnimation(animation, posX, posY);
