adfa603bd857756d964f72cfe50babf1ba5c8ecd
lordzuzen <lordzuzen@41fc50e0-9c97-0efb-add9-b61474c962b3>
2010-06-09 15:58:58 +0000

    Retocs generals a les utilitats, afegits comentaris

--- WidaDefense/src/projecte/td/utilitats/Configuracio.java
+++ WidaDefense/src/projecte/td/utilitats/Configuracio.java
@@ -1,31 +1,39 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package projecte.td.utilitats;
 
-import projecte.td.utilitats.ArxiuConfiguracio;
-
 /**
- *
- * @author wida47645633
+ * S'encarrega de carregar en memòria els diferents Arxius de Configuració i servir-los
+ * en el moment oportú
+ * @author David Alvarez Palau i Ernest Daban Macià
  */
 public class Configuracio {
 
+    // En aquest arxiu s'hi guarden les rutes dels altres fitxers de configuració
     private static ArxiuConfiguracio configuracio;
+    // Característiques dels enemics
     private static ArxiuConfiguracio enemics;
+    // Recursos amb les rutes de les imatges, audio, musica i fonts
     private static ArxiuConfiguracio recursos;
+    // Opcions generals del joc
     private static ArxiuConfiguracio opcions;
+    // Característiques de les unitats
     private static ArxiuConfiguracio unitats;
+    // Opcions del perfil 1
     private static ArxiuConfiguracio perfil1;
+    // Opcions del perfil 2
     private static ArxiuConfiguracio perfil2;
+    // Opcions del perfil 3
     private static ArxiuConfiguracio perfil3;
+    // Característiques de les waves
     private static ArxiuConfiguracio wave;
 
+    // Aquest arxiu es carregarà estaticament al iniciar l'aplicació
     static {
         configuracio = new ArxiuConfiguracio("config/td.cfg");
     }
 
+    // Els mètodes següents s'encarreguen de carregar en memòria l'ArxiuConfiguracio
+    // (en el cas que no estigui encara carregat) i a continuació retornar-lo quan
+    // aquest es sol·licitat per un altre objecte.
     public static ArxiuConfiguracio getPerfil1() {
         if (perfil1 == null) {
             perfil1 = new ArxiuConfiguracio(configuracio.getPropietatString("perfil1"));
@@ -53,7 +61,7 @@ public class Configuracio {
         }
         return wave;
     }
-    
+
     public static ArxiuConfiguracio getRecursos() {
         if (recursos == null) {
             recursos = new ArxiuConfiguracio(configuracio.getPropietatString("recursos"));
